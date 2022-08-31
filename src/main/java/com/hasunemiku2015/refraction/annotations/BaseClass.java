079f06f49316e8d2a576c7f43b46598b81841b58
BuildTools <unconfigured@null.spigotmc.org>
2022-09-01 00:07:10 +0800

    Reverted change for @BaseClass.

--- src/main/java/com/hasunemiku2015/refraction/annotations/BaseClass.java
+++ src/main/java/com/hasunemiku2015/refraction/annotations/BaseClass.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.SOURCE)
+@Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
 public @interface BaseClass {
     String name();
